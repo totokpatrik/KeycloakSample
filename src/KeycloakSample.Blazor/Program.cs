790075091164b7618e76473fcdd3d6ab5e71ef43
Tótok Patrik <totokpatrik@gmail.com>
2025-03-17 18:08:16 +0100

    working with blazor

--- src/KeycloakSample.Blazor/Program.cs
+++ src/KeycloakSample.Blazor/Program.cs
@@ -1,20 +1,23 @@
+using Keycloak.Blazor.Services;
 using KeycloakSample.Blazor;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.IdentityModel.Protocols.OpenIdConnect;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp =>
-    new HttpClient { BaseAddress = new Uri(builder.Configuration["FrontendUrl"] ?? "http://localhost:7000") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
 builder.Services.AddOidcAuthentication(options =>
 {
-    builder.Configuration.Bind("Keycloak", options.ProviderOptions);
-    options.ProviderOptions.ResponseType = OpenIdConnectResponseType.Code;
-    //options.AuthenticationPaths.LogOutSucceededPath = "";
+    options.ProviderOptions.Authority = builder.Configuration["Keycloak:auth-server-url"] + "/realms/" + builder.Configuration["Keycloak:realm"];
+    options.ProviderOptions.ClientId = builder.Configuration["Keycloak:resource"];
+    options.ProviderOptions.MetadataUrl = builder.Configuration["Keycloak:auth-server-url"] + "/realms/" + builder.Configuration["Keycloak:realm"] + "/.well-known/openid-configuration";
+
+    options.UserOptions.RoleClaim = "roles";
 });
+
+builder.Services.AddApiAuthorization().AddAccountClaimsPrincipalFactory<CustomUserFactory>();
 
 await builder.Build().RunAsync();
--- src/KeycloakSample.Blazor/wwwroot/lib/bootstrap/dist/js/bootstrap.js
+++ src/KeycloakSample.Blazor/wwwroot/lib/bootstrap/dist/js/bootstrap.js
@@ -1147,7 +1147,7 @@
     [ARROW_RIGHT_KEY$1]: DIRECTION_LEFT
   };
   const Default$b = {
-    interval: 5185,
+    interval: 5000,
     keyboard: true,
     pause: 'hover',
     ride: false,
@@ -4317,7 +4317,7 @@
   const Default = {
     animation: true,
     autohide: true,
-    delay: 5185
+    delay: 5000
   };
 
   /**
